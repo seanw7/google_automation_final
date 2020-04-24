63277168d06ea2f99d6640e92449a832d502604c
sean <seanw@protonmail.ch>
2020-04-24 02:14:21 -0700

    initial commit of first two scripts for google python automation course final test

---
+++ changeImage.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+# import PIL
+from PIL import Image
+import os
+import glob
+
+
+img_search_path = "/home/sean/Documents/courses/google_python_automation/course 6"
+resize_to = (600,400)
+img_type_to_convert = 'tiff'
+save_files_loc = '.'
+
+
+def list_of_imgfiles(folder_loc):
+    all_files = os.listdir(folder_loc)
+    print(all_files)
+    img_files = [file for file in all_files if file.endswith(img_type_to_convert)]
+    print(img_files)
+    return img_files
+
+
+def modify_image(img, img_dimensions):
+    im = Image.open(img)
+    print("Saving file: {}, at {}.jpeg".format(img, img))
+    file_name = os.path.basename(img)
+    im.resize(img_dimensions).convert("RGB").save("{}/{}.jpeg".format(save_files_loc,file_name))
+
+def main():
+    ### Grabs all files with img_type_to_convert extension ###
+    img_files = list_of_imgfiles(img_search_path)
+    ### Loops over images in array and modifys them ###
+    for img in img_files:
+        full_img_path = os.path.join(img_search_path,img)
+        modify_image(full_img_path, resize_to)
+
+main()
---
+++ example_upload.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import requests
+import os
+# This example shows how a file can be uploaded using
+# the Python Requests module
+
+
+
+
+url = "http://localhost/upload"
+type_to_convert = ('.jpg','.jpeg')
+upload_files_from = "/home/sean/Documents/courses/google_python_automation/course 6/"
+
+def list_of_imgfiles(folder_loc):
+    all_files = os.listdir(folder_loc)
+
+    img_files = [file for file in all_files if os.path.splitext(file)[1] in type_to_convert]    
+    # print(img_files)
+    return img_files
+
+def post_files(list_of_files):
+    for file in list_of_files:
+        with open(os.path.join(upload_files_from, file), 'rb') as opened:
+            print(opened)
+            #r = requests.post(url, files={'file': opened})
+
+
+files_to_upload = list_of_imgfiles(upload_files_from)
+# upload_files = 
+post_files(files_to_upload)
+
+
+
+
+
+
+
